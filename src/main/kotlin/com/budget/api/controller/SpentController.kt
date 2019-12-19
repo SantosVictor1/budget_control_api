4db3ec72dc84164e234d5d5420b4935b61a96a07
Victor Santos <victor.udi10@hotmail.com>
2019-12-19 20:47:20 -0300

    Configurações do swagger adicionadas em SpentController

--- src/main/kotlin/com/budget/api/controller/SpentController.kt
+++ src/main/kotlin/com/budget/api/controller/SpentController.kt
@@ -1,9 +1,14 @@
 package com.budget.api.controller
 
 import com.budget.api.message.request.SpentRequest
+import com.budget.api.message.response.error.ErrorResponse
 import com.budget.api.message.response.success.SpentResponse
+import com.budget.api.message.response.success.UserResponse
 import com.budget.api.service.SpentService
 import com.budget.api.service.UserService
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -16,26 +21,57 @@ class SpentController {
     @Autowired
     private lateinit var spentService: SpentService
 
+    @ApiOperation(value = "Salva gasto de um usuário")
+    @ApiResponses(
+        ApiResponse(code = 201, message = "Cadastro realizado com sucesso", response = SpentResponse::class),
+        ApiResponse(code = 400, message = "Algum dado é inválido", response = ErrorResponse::class),
+        ApiResponse(code = 401, message = "Você não está autenticado", response = ErrorResponse::class),
+        ApiResponse(code = 404, message = "Servidor não encontrado")
+    )
     @PostMapping("/spents")
     fun newSpent(@RequestBody spentRequest: SpentRequest): ResponseEntity<SpentResponse> {
-        return ResponseEntity.ok().body(spentService.saveSpent(spentRequest))
+        return ResponseEntity.status(HttpStatus.CREATED).body(spentService.saveSpent(spentRequest))
     }
 
+    @ApiOperation(value = "Retorna todos os gastos cadastrados")
+    @ApiResponses(
+        ApiResponse(code = 200, message = "Gastos retornados com sucesso", response = SpentResponse::class, responseContainer = "List"),
+        ApiResponse(code = 401, message = "Você não está autenticado", response = ErrorResponse::class),
+        ApiResponse(code = 404, message = "Servidor não encontrado")
+    )
     @GetMapping("/spents")
     fun getAll(): ResponseEntity<MutableList<SpentResponse>> {
         return ResponseEntity.ok().body(spentService.getSpents())
     }
 
+    @ApiOperation(value = "Retorna gastos de um usuário")
+    @ApiResponses(
+        ApiResponse(code = 200, message = "Gastos retornados com sucesso", response = SpentResponse::class, responseContainer = "List"),
+        ApiResponse(code = 401, message = "Você não está autenticado", response = ErrorResponse::class),
+        ApiResponse(code = 404, message = "Usuário não encontrado", response = ErrorResponse::class)
+    )
     @GetMapping("/spents/user/{userId}")
     fun getAllFromUserId(@PathVariable userId: Long): ResponseEntity<MutableList<SpentResponse>> {
         return ResponseEntity.ok().body(spentService.getBydUserId(userId))
     }
 
+    @ApiOperation(value = "Retorna um gasto pelo id")
+    @ApiResponses(
+        ApiResponse(code = 200, message = "Gasto retornado com sucesso", response = SpentResponse::class),
+        ApiResponse(code = 401, message = "Você não está autenticado", response = ErrorResponse::class),
+        ApiResponse(code = 404, message = "Gasto não encontrado", response = ErrorResponse::class)
+    )
     @GetMapping("spents/{id}")
     fun getById(@PathVariable id: Long): ResponseEntity<SpentResponse> {
         return ResponseEntity.ok().body(spentService.getById(id))
     }
 
+    @ApiOperation(value = "Deleta um gasto pelo id")
+    @ApiResponses(
+        ApiResponse(code = 204, message = "Gasto deletado com sucesso"),
+        ApiResponse(code = 401, message = "Você não está autenticado", response = ErrorResponse::class),
+        ApiResponse(code = 404, message = "Gasto não encontrado", response = ErrorResponse::class)
+    )
     @DeleteMapping("spents/{id}")
     fun deleteSpentBydId(@PathVariable id: Long): ResponseEntity<Any> {
         spentService.deleteById(id)
